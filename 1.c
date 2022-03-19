aa8ece764719c11699a4f11f01b40abcefe8f4ef
谭竣文 <92668369+lanyangyangy@users.noreply.github.com>
2022-03-19 18:12:16 +0800

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+ 
+ static int a[2];
+ *returnSize=2;
+  for(int i=0;i<numsSize-1;i++)
+  {
+ for (int j=i+1;j<numsSize;j++)
+  {
+      if(nums[i]+nums[j]==target)
+  {
+     a[0]=i;
+     a[1]=j;
+     return a;
+  }
+  }     
+  }
+return NULL;
+}
